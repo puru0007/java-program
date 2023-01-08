6f705e3231300485b980a3fcf15632084ffa5a90
prajwal pathak <prajwalpathak007@gmail.com>
2023-01-08 14:06:27 +0530

    sum

---
+++ sum.java
@@ -0,0 +1,12 @@
+import java.util.*;
+public class sum {
+    public static void main(String[] arg)
+    {
+        int a,b,c;
+        Scanner sr =new Scanner(System.in);
+        a=sr.nextInt();
+        b=sr.nextInt();
+        c=a+b;
+        System.out.println("sum is "+c);
+    }
+}
